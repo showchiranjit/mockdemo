74702f487a352f5f891065f8151b2071063dee02
showchiranjit <showchiranjit@users.noreply.github.com>
2025-03-20 07:55:54 +0530

    Update ttest.ts

--- ttest.ts
+++ ttest.ts
@@ -1,5 +1,1 @@
-I hope this email finds you well. Following our stand-up meeting with you and @Nanda, I wanted to get in touch regarding the Next.js 14 migration.
-
-As discussed, it’s important to note that Next.js 14 is strictly dependent on React 18, which means the migration to React 18 must be completed first. Additionally, since our monorepo (Nx) also depends on Next.js, this process will require some time to ensure everything is aligned and functioning correctly.
-
-Let’s plan to discuss this further and outline the necessary steps to ensure a smooth transition. Please let me know your availability so we can schedule a follow-up meeting.
+I'm working on a reactive migration. Following the switch to React 18 and a few minor code adjustments, the application functions flawlessly. The majority of test scenarios are unsuccessful. I'm working on the test cases.
